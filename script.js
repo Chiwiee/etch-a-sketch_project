946b6c1f3ebb098692ed4fff8a9e6cdfc4d30f9c
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-20 00:25:55 +0800

    Move for loop inside of getInputValue

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const body = document.querySelector("body");
 const mainContainer = document.querySelector("#main-container");
 const inputBtn = document.querySelector("#input");
 
-for (let i = 1; i <= userInput; i++) {
-  const div = document.createElement("div");
-  div.classList = "div";
-  mainContainer.appendChild(div);
-}
-
 let grid = document.querySelectorAll(".div");
 console.log(grid);
 grid.forEach((grid) => {
@@ -32,7 +26,13 @@ function userInputY() {
   }
 }
 
-function getInputValue(gridX, gridY) {}
+function getInputValue(gridX, gridY) {
+  for (let i = 1; i <= userInput; i++) {
+    const div = document.createElement("div");
+    div.classList = "div";
+    mainContainer.appendChild(div);
+  }
+}
 
 const gridX = userInputX();
 console.log(gridX);
