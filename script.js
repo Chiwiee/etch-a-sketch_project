574f36bb6b67b5a8457e4ef6cf3167086fec1d97
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-19 19:34:04 +0800

    Add createElement div

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const body = document.querySelector("body");
 const mainContainer = document.querySelector("#main-container");
 
-for (let i = 1; i <= 16 * 16; i++) {}
+for (let i = 1; i <= 16 * 16; i++) {
+  const div = document.createElement("div");
+}
