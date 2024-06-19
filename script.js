c8ffb17115e9ab5caaabadbb189008f74ef2b353
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-19 19:34:24 +0800

    Add mainContainer appendChild div

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const mainContainer = document.querySelector("#main-container");
 
 for (let i = 1; i <= 16 * 16; i++) {
   const div = document.createElement("div");
+  mainContainer.appendChild(div);
 }
