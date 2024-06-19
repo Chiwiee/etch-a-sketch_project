3a1ffc372599520bf1e9f6405dec536d26f04fe3
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-19 19:56:59 +0800

    Change i value to x string

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const mainContainer = document.querySelector("#main-container");
 
 for (let i = 1; i <= 16 * 16; i++) {
   const div = document.createElement("div");
-  div.textContent = `x`;
+  div.textContent = `${"x"}`;
   mainContainer.appendChild(div);
 }
 
