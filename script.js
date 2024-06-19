e1315366ff44821ea8804ecb92b6d3a5862894a1
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-19 23:14:42 +0800

    Add input button querySelector

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const body = document.querySelector("body");
 const mainContainer = document.querySelector("#main-container");
+const inputBtn = document.querySelector("#input");
 
 for (let i = 1; i <= userInput; i++) {
   const div = document.createElement("div");
