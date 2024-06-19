45669b1b68cf47cf98b78e112a7615b26b058471
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-19 19:39:07 +0800

    Add #main-container display flex: git commit -am Add

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mainContainer = document.querySelector("#main-container");
 
 for (let i = 1; i <= 16 * 16; i++) {
   const div = document.createElement("div");
+  div.textContent = `x`;
   mainContainer.appendChild(div);
 }
 
