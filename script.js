26df4ec200fdc634000e99418ba27a82352b0f15
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-19 23:45:13 +0800

    Remove addEventListener and console.log

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ for (let i = 1; i <= userInput; i++) {
   mainContainer.appendChild(div);
 }
 
-console.log(mainContainer);
-
 let grid = document.querySelectorAll(".div");
 console.log(grid);
 grid.forEach((grid) => {
@@ -17,15 +15,3 @@ grid.forEach((grid) => {
     e.target.style.background = "black";
   });
 });
-
-function userInput() {
-  let input = prompt("Please Enter a Number");
-  let maximum = 100;
-  if (input < maximum) {
-    return input;
-  } else if (input > maximum) {
-    return (input = prompt("Please Enter a Number"));
-  }
-}
-
-let userInput = userInput();
