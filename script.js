d5c8d63f8ae0ff61ec0c5c948ca5be21b2413995
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-20 21:17:56 +0800

    Add mainBox createelement div and class

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const body = document.querySelector("body");
 const mainContainer = document.querySelector("#main-container");
 const inputBtn = document.querySelector("#input");
+const box = document.createElement("div");
+box.classList = "mainBox";
 
 for (let i = 0; i < 16 * 16; i++) {
   const mainGrid = document.createElement("div");
